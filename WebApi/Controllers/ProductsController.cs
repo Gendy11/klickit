3589839c423df3f65daea9fe64df3c1799d11f9e
Gendy117 <ahmedelgendy1170@gmail.com>
2022-09-02 03:34:20 +0200

    End of section 6

--- WebApi/Controllers/ProductsController.cs
+++ WebApi/Controllers/ProductsController.cs
@@ -11,6 +11,7 @@ using WebApi.Dtos;
 using AutoMapper;
 using WebApi.Errors;
 using Microsoft.AspNetCore.Http;
+using WebApi.Helpers;
 
 namespace WebApi.Controllers
 {
@@ -33,13 +34,18 @@ namespace WebApi.Controllers
 
         [HttpGet]
 
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<Pagination<IReadOnlyList<ProductToReturnDto>>>> GetProducts(
+           [FromQuery]ProductSpecParams productParams)
         {
-            var spec=new ProductsWithTypesAndBrandsSpecification();
+            var spec=new ProductsWithTypesAndBrandsSpecification(productParams);
+            var countSpec=new ProductWithFiltersForCountSpecification(productParams);
+            var totalItems=await _productsRepo.CountAsync(countSpec);
             var products= await _productsRepo.ListAsync(spec);
+            var data=_mapper.Map<IReadOnlyList<Product>,IReadOnlyList<ProductToReturnDto>>(products);
     
 
-            return Ok(_mapper.Map<IReadOnlyList<Product>,IReadOnlyList<ProductToReturnDto>>(products));
+            return Ok(new Pagination<ProductToReturnDto>(productParams.pageIndex,productParams.PageSize,
+                totalItems,data));
         }
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
